366edbd4d22e23a1adbc51dd80246bc0a0a65d19
Paul van Schayck <paul.van.schayck@knmi.nl>
2023-01-13 15:15:59 +0100

    Remove ENVIRONMENT_SUFFIX logic

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import logging
 import os
 
@@ -7,18 +6,6 @@ import setuptools
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
-
-# Package meta-data.
-NAME = "covjson-pydantic"
-
-# TODO: See what we need to do about branches
-env_suffix = os.environ.get("ENVIRONMENT_SUFFIX", "")
-logger.debug(f"Environment suffix: {env_suffix}")
-
-if env_suffix:
-    NAME += f"-{env_suffix}"
-logger.debug(f"Package name: {NAME}")
-
 
 if __name__ == "__main__":
     setuptools.setup()
